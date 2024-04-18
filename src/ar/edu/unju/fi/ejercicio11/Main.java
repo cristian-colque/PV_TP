dc413dbcfcc430e5022befa6703b3b698316e101
cristian <melopartituras@gmail.com>
2024-04-18 12:55:42 -0300

    Se termino el ejercicio 11

---
+++ src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 2;
+        int j = 40;
+
+        for (; j > 0; j--, i++) {
+            
+                System.out.print((i*j) + " ");
+        }
+
+	}
+
+}
+
