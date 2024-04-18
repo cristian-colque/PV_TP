d1027967aa37e1591dab20495513a3bbab227479
cristian <melopartituras@gmail.com>
2024-04-18 17:31:26 -0300

    Ejercicio 13

--- src/ar/edu/unju/fi/ejercicio1/Main.java
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,14 +3,11 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		int num1, num2, num3, num4, num5;
-		
-		num1 = (int) (Math.random() * 101);
-		num2 = (int) (Math.random() * 101);
-		num3 = (int) (Math.random() * 101);
-		num4 = (int) (Math.random() * 101);
-		num5 = (int) (Math.random() * 101);
+		int num1 = (int) (Math.random() * 101);
+		int num2 = (int) (Math.random() * 101);
+		int num3 = (int) (Math.random() * 101);
+		int num4 = (int) (Math.random() * 101);
+		int num5 = (int) (Math.random() * 101);
 		
 		int resultado = calcularPromedio(num1, num2, num3,num4, num5);
 		mostrarNumeros(num1, num2, num3,num4, num5);
@@ -22,7 +19,9 @@ public class Main {
 		
 		int cantidadParametros = numeros.length;
         switch (cantidadParametros) {
-           
+            case 0:
+                System.out.println("No se han proporcionado parámetros.");
+                break;
             case 1:
                 System.out.println(numeros[0]);
                 break;
@@ -31,7 +30,7 @@ public class Main {
         		
                 break;
             default:
-                System.out.println("Error de parametros");
+                System.out.println("Se proporcionaron parámetros incorrectos.");
            
         }
 		
---
+++ src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio13;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[8];
+        
+  
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingrese el valor para la posición " + i + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+    
+        System.out.println("Índice\tValor");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(i + "\t" + numeros[i]);
+        }
+        
+        scanner.close();
+
+	}
+
+}
